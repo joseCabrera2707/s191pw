dd7959029882084b7bcadd6b6c3cb6a3532f038e
AXE1GR <ramirezoaxel@gmail.com>
2024-09-26 19:53:40 -0600

    Práctica 3 Avance rutas

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::view('/','inicio')->name('rutainicio');
+
+Route::view('/form','formulario')->name('rutacacas');
+
+Route::view('/consultar','clientes')->name('rutaconsulta');
