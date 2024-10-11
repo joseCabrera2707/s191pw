d11abee3a765dcc4bfb7da4bf8668f7fa39a219a
Jose Cabrera <122041957@upq.edu.mx>
2024-10-10 19:46:46 -0600

    Práctica 3.1 terminada

--- IntroLaravel/resources/views/components/alert.blade.php
+++ IntroLaravel/resources/views/components/alert.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+<div class="container mt-4">
+<div class='alert alert-{{$tipo}}' role='alert'>
+    Soy un alert {{$slot}}
+</div>
 </div>
